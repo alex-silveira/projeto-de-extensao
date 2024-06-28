556dc5fb69c8c2f07bcfd39396368b0d3c0c67f4
alex <alexarauj0015@hotmail.com>
2024-06-28 15:56:21 -0300

    Add: column gender

--- database/migrations/2024_06_13_200314_create_desemprego_table.php
+++ database/migrations/2024_06_13_200314_create_desemprego_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');       
             $table->string('firstname');
             $table->string('lastname');
+            $table->string('gender');
             $table->string('phone');
             $table->date('birthdate');
             $table->string('address');
